45dd7fbcb626240a6ee458368607583884328d84
Omar Thioune <2326389@cmontmorency.qc.ca>
2025-09-12 15:04:55 -0400

    Vue sport

---
+++ Vue/Sport/index.php
@@ -0,0 +1,20 @@
+<?php $this->titre = 'Organisme de sport'; ?>
+
+<a href="Sport/CreateSport">
+    <h2 class="titreArticle">Ajouter un nouveau sport</h2>
+</a>
+<?php foreach ($sports as $sport):
+    ?>
+
+    <article>
+        <header>
+            <a href="Sport/ShowSport/<?= $sport['id'] ?>">
+                <h1 class="titreArticle"><?= $sport['name'] ?></h1>
+            </a>
+        </header>
+        <p><?= $sport['description'] ?><br/>
+            <a href="Sport/EditSport/<?= $sport['id'] ?>"> [modifier le sport]</a>
+        </p>
+    </article>
+    <hr />
+<?php endforeach; ?>    
