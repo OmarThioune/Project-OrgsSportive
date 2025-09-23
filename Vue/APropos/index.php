540bf5ae6c9d1dd9a78e74433c6c7d4356865b3d
MasterOfTheGame <124705060+Master7427@users.noreply.github.com>
2025-09-23 15:24:26 -0400

    A Propos : index.php

---
+++ Vue/APropos/index.php
@@ -0,0 +1,35 @@
+<?php $titre = 'A propos Organisation Sportive'; ?>
+ <p>Huriel Georges Pierre et Omar Thioune</p>
+<ul>
+    <li>420-5H6 MO Application Web transactionnelles</li>
+    <li>Automne 2025, Collège Montmorency</li>
+</ul>
+<h3>Organisation Sportive?</h3>
+<ul>
+    <li>Organisation Sportive est une application web qui permet de gérer les informations relatives aux organisations sportives, y compris les équipes, les joueurs, les matchs et les statistiques.</li>
+    <li>La page d'Accueil présente la liste sports avec une description et une equipe sportive
+    <li>On y retrouve un lien pour se connecte </li>
+    <ul>
+        <li>Un utilisateur peut se connecter en utilisant son nom d'utilisateur et son mot de passe.</li>
+        <li>Une fois connecté, l'utilisateur peut accéder à des fonctionnalités supplémentaires telles que la gestion des équipes, des joueurs et des matchs.</li>
+</ul>
+</p>
+<h3>Technologies Utilisées</h3>
+<ul>
+    <li>PHP</li>
+    <li>HTML/CSS</li>
+</ul>
+<h3>Fonctionnalités</h3>
+<ul>
+    <li>Authentification : Système de connexion pour les utilisateurs afin de sécuriser l'accès aux fonctionnalités avancées.</li>
+    <li>Interface utilisateur conviviale : Conception responsive pour une utilisation sur différents appareils.</li>
+</ul>
+<h3>Contributeurs</h3>
+<ul>
+    <li>Huriel Georges Pierre - Développeur principal</li>
+    <li>Omar Thioune - Développeur principal</li>
+</ul>
+<h3>Contact</h3>
+<p>Pour toute question ou suggestion, veuillez nous contacter à l'adresse email suivante : contact@organisationsportive.ca <a href="mailto:contact@orgsportive.com">contact@orgsportive.com</a></p>
+<p>Merci d'utiliser Organisation Sportive!</p>
+
